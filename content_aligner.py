83f2ada76911936f835c5f6d929456196f421cb5
alyssa6520 <2269024334@qq.com>
2026-09-13 06:43:51 +0800

    Add files via upload

--- content_aligner.py
+++ content_aligner.py
@@ -28,7 +28,7 @@ class ContentAligner:
                 base_url=Config.OPENAI_BASE_URL
             )
             all_embeds = []
-            batch_size = 10
+            batch_size = 5
             for i in range(0, len(texts), batch_size):
                 batch = texts[i:i + batch_size]
                 response = client.embeddings.create(
